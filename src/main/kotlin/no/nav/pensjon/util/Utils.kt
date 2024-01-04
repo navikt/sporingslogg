d16a7b81afe827dde0a9536737cf2de7f09d8350
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2024-01-04 13:51:25 +0100

    Oppdatert flere dependencies.

--- src/main/kotlin/no/nav/pensjon/util/Utils.kt
+++ src/main/kotlin/no/nav/pensjon/util/Utils.kt
@@ -2,6 +2,7 @@ package no.nav.pensjon.util
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
@@ -10,8 +11,9 @@ fun String?.scrable() = if (this == null || this.length < 6 ) this else this.dro
 inline fun <reified T : Any> typeRefs(): TypeReference<T> = object : TypeReference<T>() {}
 
 inline fun <reified T : Any> fromJson2Any(json: String, typeRef: TypeReference<T> = typeRefs(), failonunknown: Boolean = true): T {
+    @Suppress("CAST_NEVER_SUCCEEDS")
     return jacksonObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, failonunknown)
-            .registerModule(JavaTimeModule())
-            .readValue(json, typeRef)
+        .registerModule(JavaTimeModule() as Module)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, failonunknown)
+        .readValue(json, typeRef)
 }
